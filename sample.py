cb69d919a2c52cd4c68dd6a1f82bdecc8efe4f6f
xuegh <1625193698@qq.com>
2018-09-09 11:36:29 +0800

    6

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@
 //edit 2nd for test 2018-9-8
 //edit 3rd 4th
 //5 edit under branch dev
+//6 edit on new branch feature1
 import time,traceback,os,logging,pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
