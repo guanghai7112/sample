f40623a4823897b1041be933a14298d7dc21d39c
xuegh <1625193698@qq.com>
2018-09-09 11:28:47 +0800

    commit on dev

---
+++ sample.py
@@ -0,0 +1,13 @@
+//edit for git test 2018-9-8
+//edit 2nd for test 2018-9-8
+//edit 3rd 4th
+//5 edit under branch dev
+import time,traceback,os,logging,pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+logging.basicConfig(level=logging.INFO,
+format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+datefmt='%a, %d %b %Y %H:%M:%S',
+filename='C:/my/CloudStation/py/log/login91.log',
+filemode='a')
